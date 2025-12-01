d0508efa16a98edd1b0dfcf27561895cf5febc0a
Pouria-Jamshidi <the.eye.programming@gmail.com>
2025-12-01 19:59:18 +0330

    6th mordad-karokamp codings during class

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from core.views import user_list,jadid,main_page
+from core.views import user_list, posts, main_page, post_detail, new_post
 
 urlpatterns = [
-    path('',main_page,name='main page'),
-    path('posts/',jadid,name='posts'),
+    path('', main_page, name='Home'),
+    path('posts/', posts, name='posts'),
     path('users/',user_list,name='users'),
+    path('posts/post/<int:post_id>/',post_detail,name='post_detail'),
+    path('post/new/',new_post,name='new_post'),
 
 ]
--- core/views.py
+++ core/views.py
@@ -3,9 +3,9 @@ from django.shortcuts import render
 from core.models import Post,User
 
 
-def jadid(request):
+def posts(request):
     p=Post.objects.all()
-    return render(request, 'core/home.html', context={'posts':p})
+    return render(request, 'core/posts.html', context={'posts':p})
 
 def user_list(request):
     u = User.objects.all()
@@ -14,3 +14,9 @@ def user_list(request):
 def main_page(request):
     return render(request,'core/main.html')
 # Create your views here.
+def post_detail(request,post_id):
+    post = Post.objects.get(pk=post_id)
+    return render(request, 'core/post_detail.html', {'post':post})
+
+def new_post(request):
+    return render(request, 'core/new_post.html')
