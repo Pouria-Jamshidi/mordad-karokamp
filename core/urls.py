d4ddf46f5a2a3d2742052b91d438df95b2fd0209
Pouria-Jamshidi <the.eye.programming@gmail.com>
2025-12-15 16:59:09 +0330

    class session 9 : model form

--- core/urls.py
+++ core/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from core.views import user_list, posts, main_page, post_detail, new_post, new_user, user_detail
+from core.views import user_list, posts, main_page, post_detail, new_post, new_user, user_detail, delete_post
 
 urlpatterns = [
     path('', main_page, name='Home'),
     path('posts/', posts, name='posts'),
     path('posts/post/<int:post_id>/', post_detail, name='post_detail'),
     path('post/new/', new_post, name='new_post'),
+    path('post/delete/<int:post_id>/',delete_post, name='delete_post'),
     path('users/', user_list, name='users'),
     path('users/user/<int:user_id>/', user_detail, name='user_detail'),
     path('user/new', new_user, name='new_user'),
